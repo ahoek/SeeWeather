7d791f762596e6241c8482211b93bc30d38c3a21
Arthur Hoek <arthur@arthurhoek.nl>
2014-04-09 21:56:13 +0200

    fix weather service

--- platforms/ios/www/js/services.js
+++ platforms/ios/www/js/services.js
@@ -1,16 +1,15 @@
 angular.module('starter.services', [])
 
 	/**
-	 * A simple example service that returns some data.
+	 * Location service
 	 */
-	.factory('Friends', function() {
-		// Might use a resource here that returns a JSON array
-
-		// Location placeholder
+	.factory('Locations', function($q, $http) {
+		// Location placeholder, @todo get from local storage
 		var locations = [
-			{id: 0, name: 'Wassenaar, nl', weather: {} },
-			{id: 1, name: 'Den Haag, nl', weather: {} },
-			{id: 2, name: 'Voorschoten, nl', weather: {} }
+			{id: 0, name: 'Wassenaar, nl' },
+			{id: 1, name: 'Den Haag, nl' },
+			{id: 2, name: 'Voorschoten, nl' },
+			{id: 3, name: 'Rotterdam, nl' }
 		];
 
 		return {
@@ -19,30 +18,29 @@ angular.module('starter.services', [])
 			},
 			get: function(locationId) {
 				var location = locations[locationId];
-				
-				// Simple index lookup
-				var openWeatherUrl = "http://api.openweathermap.org/data/2.5/weather";
-				// @todo make async
-				//jQuery.ajaxSetup({async:false});
-//				jQuery.get(openWeatherUrl, { 
-//					APPID: "***REMOVED***",
-//					q: location.name 
-//				}, function(weatherData) {
-//					location.weather = weatherData;
-//					console.log(location);
-//				});
-//$http({method: 'GET', url: openWeatherUrl}).
-//    success(function(data, status, headers, config) {
-//      // this callback will be called asynchronously
-//      // when the response is available
-//	  location.weather = data;
-//    }).
-//    error(function(data, status, headers, config) {
-//      // called asynchronously if an error occurs
-//      // or server returns response with an error status.
-//    });
-console.log(location);
 				return location;
+			},
+			// Get the current weather
+			getWeather: function(name) {
+				var deferred = $q.defer();
+				var openWeatherUrl = "http://api.openweathermap.org/data/2.5/weather";
+
+				$http({
+					method: 'GET', 
+					url: openWeatherUrl, 
+					params: {
+						APPID: "***REMOVED***",
+						q: name						
+					}}).
+					success(function(data, status, headers, config) {
+					  deferred.resolve(data);
+					}).
+					error(function(data, status, headers, config) {
+						console.log('jammer maar helaas');
+					});
+		
+                return deferred.promise;
+				
 			}
 		}
 	});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,4 +6,7 @@ angular.module('starter.controllers', [])
 
 	.controller('LocationController', function($scope, $stateParams, Locations) {
 		$scope.location = Locations.get($stateParams.locationId);
+	        Locations.getWeather($scope.location.name).then(function(weather) {
+            $scope.weather = weather;
+        });
 	})
