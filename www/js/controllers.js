b1e7a351f32ad576287eaea96304f7715d0dd041
Arthur Hoek <arthur@arthurhoek.nl>
2014-04-12 11:19:09 +0200

    add forecast dividers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,23 +3,45 @@ angular.module('starter.controllers', [])
 	.controller('LocationsController', function($scope, Locations) {
 		$scope.locations = Locations.all();
 	})
-	
+
 	.controller('LocationController', function($scope, $stateParams, Locations) {
 		$scope.location = Locations.get($stateParams.locationId);
+
+		var makeGroups = function(weatherList) {
+			var forecastDays = {};
+			var forecastsLength = weatherList.length;
+			var day;
+
+			for (var i = 0; i < forecastsLength; i++) {
+				day = new Date(weatherList[i].dt * 1000).toISOString().replace(/T.+/, '');
+
+				if (!forecastDays[day])
+					forecastDays[day] = [];
+
+				forecastDays[day].push(weatherList[i]);
+			}
+			return forecastDays;
+		};
+
+		
 		Locations.getWeather($scope.location.name).then(function(weather) {
-			$scope.weather = weather.list;
+			//$scope.weather = weather.list;
+			$scope.forecastDays = makeGroups(weather.list);
+
+
 		});
+
 		$scope.refreshWeather = function() {
 			Locations.getWeather($scope.location.name).then(function(weather) {
-				$scope.weather = weather.list;
+				$scope.forecastDays = makeGroups(weather.list);
 
-				var now = new Date();
-				$scope.lastRefresh = "Laatste update: " + now.toTimeString();
 				//Stop the ion-refresher from spinning
 				$scope.$broadcast('scroll.refreshComplete');
 			});
 		};
 	})
+
+	// Side menu toggle button
 	.controller('ContentController', function($scope, $ionicSideMenuDelegate) {
 		$scope.toggleRight = function() {
 			$ionicSideMenuDelegate.toggleRight();
