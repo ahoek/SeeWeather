806a0756ab37d950236476b2500c704c37f14120
Arthur Hoek <arthur@arthurhoek.nl>
2014-10-21 23:13:41 +0200

    Add moving cloud background for item rows

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,119 +1,119 @@
 angular.module('starter.controllers', [])
-    .controller('AppController', function($scope, $ionicSideMenuDelegate) {
-      $scope.toggleLeft = function() {
-        $ionicSideMenuDelegate.toggleLeft();
-      };
+    .controller('AppController', function ($scope, $ionicSideMenuDelegate) {
+        $scope.toggleLeft = function () {
+            $ionicSideMenuDelegate.toggleLeft();
+        };
     })
 
     // Location overview
-    .controller('LocationsController', function($scope, Locations, $ionicModal, $ionicSideMenuDelegate) {
+    .controller('LocationsController', function ($scope, Locations, $ionicModal, $ionicSideMenuDelegate) {
 
-      $scope.locations = Locations.all();
-      console.log($scope.locations);
+        $scope.locations = Locations.all();
+        console.log($scope.locations);
 
-      // Grab the last active, or the first location
-      $scope.activeLocation = $scope.locations[Locations.getLastActiveIndex()];
+        // Grab the last active, or the first location
+        $scope.activeLocation = $scope.locations[Locations.getLastActiveIndex()];
 
-      // Create and load the Modal
-      $ionicModal.fromTemplateUrl('templates/new-location.html', function(modal) {
-        $scope.locationModal = modal;
-      }, {
-        scope: $scope,
-        animation: 'slide-in-up'
-      });
-
-      // Called when the form is submitted
-      $scope.createLocation = function(location) {
-        $scope.locations.push({
-          name: location.name,
-          id: location.id
+        // Create and load the Modal
+        $ionicModal.fromTemplateUrl('templates/new-location.html', function (modal) {
+            $scope.locationModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
         });
-        Locations.save($scope.locations);
-        $scope.locationModal.hide();
-        location.name = "";
-        location.id = null;
-      };
 
-      // Open our new location modal
-      $scope.newLocation = function() {
-        $scope.locationModal.show();
-      };
+        // Called when the form is submitted
+        $scope.createLocation = function (location) {
+            $scope.locations.push({
+                name: location.name,
+                id: location.id
+            });
+            Locations.save($scope.locations);
+            $scope.locationModal.hide();
+            location.name = "";
+            location.id = null;
+        };
 
-      // Close the new location modal
-      $scope.closeNewLocation = function() {
-        $scope.locationModal.hide();
-      };
+        // Open our new location modal
+        $scope.newLocation = function () {
+            $scope.locationModal.show();
+        };
 
-      $scope.removeLocation = function(location) {
-        var index = $scope.locations.indexOf(location);
-        $scope.locations.splice(index, 1);
-        Locations.save($scope.locations);
-      };
+        // Close the new location modal
+        $scope.closeNewLocation = function () {
+            $scope.locationModal.hide();
+        };
 
-      // Called to select the given location
-      $scope.selectLocation = function(location) {
-        $scope.activeLocation = location;
-        Locations.setLastActiveIndex(location.id);
-        $ionicSideMenuDelegate.toggleLeft(false);
-      };
+        $scope.removeLocation = function (location) {
+            var index = $scope.locations.indexOf(location);
+            $scope.locations.splice(index, 1);
+            Locations.save($scope.locations);
+        };
+
+        // Called to select the given location
+        $scope.selectLocation = function (location) {
+            $scope.activeLocation = location;
+            Locations.setLastActiveIndex(location.id);
+            $ionicSideMenuDelegate.toggleLeft(false);
+        };
     })
 
     // Location forecast detail
-    .controller('LocationController', function($scope, $stateParams, Locations, OpenWeatherMap) {
-      $scope.location = Locations.get($stateParams.locationId);
+    .controller('LocationController', function ($scope, $stateParams, Locations, OpenWeatherMap) {
+        $scope.location = Locations.get($stateParams.locationId);
 
-      // Group by day helper function
-      var makeGroups = function(weatherList) {
-        var forecastDays = {};
-        var forecastsLength = weatherList.length;
-        var day;
+        // Group by day helper function
+        var makeGroups = function (weatherList) {
+            var forecastDays = {};
+            var forecastsLength = weatherList.length;
+            var day;
 
-        for (var i = 0; i < forecastsLength; i++) {
-          // Convert timestamp to iso date
-          day = new Date(weatherList[i].dt * 1000).toISOString().replace(/T.+/, '');
+            for (var i = 0; i < forecastsLength; i++) {
+                // Convert timestamp to iso date
+                day = new Date(weatherList[i].dt * 1000).toISOString().replace(/T.+/, '');
 
-          if (!forecastDays[day]) {
-            forecastDays[day] = [];
-          }
-          forecastDays[day].push(weatherList[i]);
+                if (!forecastDays[day]) {
+                    forecastDays[day] = [];
+                }
+                forecastDays[day].push(weatherList[i]);
+            }
+            return forecastDays;
+        };
+
+        if ($scope.location) {
+            OpenWeatherMap.getWeather($scope.location).then(function (weather) {
+                $scope.forecastDays = makeGroups(weather.list);
+            });
         }
-        return forecastDays;
-      };
 
-      if ($scope.location) {
-        OpenWeatherMap.getWeather($scope.location.name).then(function(weather) {
-          $scope.forecastDays = makeGroups(weather.list);
-        });
-      }
+        $scope.refreshWeather = function () {
+            OpenWeatherMap.getWeather($scope.location).then(function (weather) {
+                $scope.forecastDays = makeGroups(weather.list);
 
-      $scope.refreshWeather = function() {
-        OpenWeatherMap.getWeather($scope.location.name).then(function(weather) {
-          $scope.forecastDays = makeGroups(weather.list);
-
-          //Stop the ion-refresher from spinning
-          $scope.$broadcast('scroll.refreshComplete');
-        });
-      };
+                //Stop the ion-refresher from spinning
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        };
     })
 
     // Add location form
-    .controller('LocationFormController', function($scope, Geo, OpenWeatherMap) {
-  
-      $scope.getNearbyCities = function() {
-        // Get the geo position from the device
-        Geo.getLocation().then(function(position) {
-          console.log(position.coords);
-          // Get the location from openweathermap
-          OpenWeatherMap.findLocation(position.coords).then(function(cities) {
-            $scope.nearbyCities = cities;
+    .controller('LocationFormController', function ($scope, Geo, OpenWeatherMap) {
 
-          });
-        });
-      };   
- 
-      $scope.setCity = function (city) {
-        $scope.city = city;
-      };
+        $scope.getNearbyCities = function () {
+            // Get the geo position from the device
+            Geo.getLocation().then(function (position) {
+                console.log(position.coords);
+                // Get the location from openweathermap
+                OpenWeatherMap.findLocation(position.coords).then(function (cities) {
+                    $scope.nearbyCities = cities;
+
+                });
+            });
+        };
+
+        $scope.setCity = function (city) {
+            $scope.city = city;
+        };
     })
 
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,138 +1,139 @@
 angular.module('starter.services', [])
-  /**
-   * Geolocation
-   * 
-   * @param {type} $q
-   * @returns {_L5.Anonym$1}
-   */
-  .factory('Geo', function($q) {
-    return {
-      getLocation: function() {
-        var deferred = $q.defer();
-        navigator.geolocation.getCurrentPosition(function(position) {
-          deferred.resolve(position);
-        }, function(error) {
-          deferred.reject(error);
-        });
-        return deferred.promise;
-      }
-    }
-  })
-  /**
-   * Locations and weather services
-   */
-  .factory('Locations', function() {
-
-    return {
-      all: function() {
-        var locationsString = window.localStorage['locations'];
-        if (locationsString) {
-          return angular.fromJson(locationsString);
-        }
-        return [];
-      },
-
-      get: function(locationId) {
-        locationId = parseInt(locationId);
-        var locationsString = window.localStorage['locations'];
-        if (locationsString) {
-          var locations = angular.fromJson(locationsString);
-          for (var index = 0; index < locations.length; index++) {
-            if (locations[index].id === locationId) {
-              return locations[index];
-            }
-          }
-        }
-        return {};
-      },
-
-      save: function(locations) {
-        window.localStorage['locations'] = angular.toJson(locations);
-      },
-
-      // Todo verify location and store location identifier
-      newLocation: function(name) {
-        // Add a new location
+    /**
+     * Geolocation
+     * 
+     * @param {type} $q
+     * @returns {_L5.Anonym$1}
+     */
+    .factory('Geo', function($q) {
         return {
-          name: name,
-          id: null
-        };
-      },
-
-      getLastActiveIndex: function() {
-        return parseInt(window.localStorage['lastActiveLocation']) || 0;
-      },
-
-      setLastActiveIndex: function(id) {
-        window.localStorage['lastActiveLocation'] = id;
-      } 
-    };
-  })
-  
-  /**
-   * OpenWeatherMap service
-   */
-  .factory('OpenWeatherMap', function($q, $http) {
-    var openWeatherBaseUrl = "http://api.openweathermap.org/data/2.5";
-    var appId = OPENWEATHERMAP_APPID;
-
-    return {
-       // Find a location with geo coordinates
-      findLocation: function(coords) {
-        var deferred = $q.defer();
-        var url = openWeatherBaseUrl + "/find";
-        var params = {
-          APPID: appId,
-          lat: coords.latitude.toFixed(6),
-          lon: coords.longitude.toFixed(6),
-          cnt: 4,
-          type: 'like',
-          mode: "json"
-        };
-        console.log(params);
-        $http({
-          method: 'GET',
-          url: url,
-          params: params}).
-          success(function(data, status, headers, config) {
-            console.log(data, status);
-            var location = {};
-            if (data.count > 0) {
-              var locations = data.list;
-              deferred.resolve(locations);
-            } else {
-              deferred.reject('Error finding city with given coordinates');
+            getLocation: function() {
+                var deferred = $q.defer();
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    deferred.resolve(position);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+                return deferred.promise;
             }
-          }).
-          error(function(data, status, headers, config) {
-            console.log(data, status);
-            deferred.reject('Error finding city');
-          });
+        }
+    })
+    /**
+     * Locations and weather services
+     */
+    .factory('Locations', function() {
 
-        return deferred.promise;
-      },
+        return {
+            all: function() {
+                var locationsString = window.localStorage['locations'];
+                if (locationsString) {
+                    return angular.fromJson(locationsString);
+                }
+                return [];
+            },
+            get: function(locationId) {
+                locationId = parseInt(locationId);
+                var locationsString = window.localStorage['locations'];
+                if (locationsString) {
+                    var locations = angular.fromJson(locationsString);
+                    for (var index = 0; index < locations.length; index++) {
+                        if (locations[index].id === locationId) {
+                            return locations[index];
+                        }
+                    }
+                }
+                return {};
+            },
+            save: function(locations) {
+                window.localStorage['locations'] = angular.toJson(locations);
+            },
+            // Todo verify location and store location identifier
+            newLocation: function(name) {
+                // Add a new location
+                return {
+                    name: name,
+                    id: null
+                };
+            },
+            getLastActiveIndex: function() {
+                return parseInt(window.localStorage['lastActiveLocation']) || 0;
+            },
+            setLastActiveIndex: function(id) {
+                window.localStorage['lastActiveLocation'] = id;
+            }
+        };
+    })
 
-      // Get the weather forecast
-      getWeather: function(name) {
-        var deferred = $q.defer();
-        //var openWeatherUrl = openWeatherBaseUrl + "/weather";
-        //var openWeatherUrl = openWeatherBaseUrl + "/forecast/daily";
-        var openWeatherUrl = openWeatherBaseUrl + "/forecast";
-        $http({
-          method: 'GET',
-          url: openWeatherUrl,
-          params: {
-            APPID: appId,
-            q: name
-          }}).
-          success(function(data, status, headers, config) {
-            deferred.resolve(data);
-          }).
-          error(function(data, status, headers, config) {
-            deferred.reject('Error retrieving weather forecast');
-          });
+    /**
+     * OpenWeatherMap service
+     */
+    .factory('OpenWeatherMap', function($q, $http) {
+        var openWeatherBaseUrl = "http://api.openweathermap.org/data/2.5";
+        var appId = OPENWEATHERMAP_APPID;
 
-        return deferred.promise;
-      }
-    };   
-  });
+        return {
+            // Find a location with geo coordinates
+            findLocation: function(coords) {
+                var deferred = $q.defer();
+                var url = openWeatherBaseUrl + "/find";
+                var params = {
+                    APPID: appId,
+                    lat: coords.latitude.toFixed(6),
+                    lon: coords.longitude.toFixed(6),
+                    cnt: 4,
+                    type: 'like',
+                    mode: "json"
+                };
+                console.log(params);
+                $http({
+                    method: 'GET',
+                    url: url,
+                    params: params}).
+                    success(function(data, status, headers, config) {
+                        console.log(data, status);
+                        var location = {};
+                        if (data.count > 0) {
+                            var locations = data.list;
+                            deferred.resolve(locations);
+                        } else {
+                            deferred.reject('Error finding city with given coordinates');
+                        }
+                    }).
+                    error(function(data, status, headers, config) {
+                        console.log(data, status);
+                        deferred.reject('Error finding city');
+                    });
+
+                return deferred.promise;
+            },
+            /** 
+             * Get the weather forecast
+             */
+            getWeather: function(location) {
+                var deferred = $q.defer();
+                var openWeatherUrl = openWeatherBaseUrl + "/forecast";
+                var params = {
+                    APPID: appId
+                };
+                if (location.id) {
+                    params.id = location.id;
+                } else if (location.name) {
+                    params.q = location.name;
+                } else {
+                    deferred.reject('Invalid location object input');
+                }
+                $http({
+                    method: 'GET',
+                    url: openWeatherUrl,
+                    params: params}).
+                    success(function(data, status, headers, config) {
+                        deferred.resolve(data);
+                    }).
+                    error(function(data, status, headers, config) {
+                        deferred.reject('Error retrieving weather forecast');
+                    });
+
+                return deferred.promise;
+            }
+        };
+    });
